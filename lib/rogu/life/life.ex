d45706da3cfc2a2fbbc07095b411ad3a77ace4a2
AquarHEAD Lou <aquarhead@ela.build>
2018-05-29 22:20:27 +0000

    Get display_date in get_log!/1

--- lib/rogu/life/life.ex
+++ lib/rogu/life/life.ex
@@ -21,31 +21,14 @@ defmodule Rogu.Life do
 
   @doc """
   Gets a single log.
-
-  Raises `Ecto.NoResultsError` if the Log does not exist.
-
-  ## Examples
-
-      iex> get_log!(123)
-      %Log{}
-
-      iex> get_log!(456)
-      ** (Ecto.NoResultsError)
-
   """
-  def get_log!(id), do: Repo.get!(Log, id)
+  def get_log!(id) do
+    query = Log |> select([l], %{l | display_date: type(fragment("COALESCE(detsu, inserted_at)"), :date)})
+    Repo.get!(query, id)
+  end
 
   @doc """
   Creates a log.
-
-  ## Examples
-
-      iex> create_log(%{field: value})
-      {:ok, %Log{}}
-
-      iex> create_log(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def create_log(attrs \\ %{}) do
     %Log{}
@@ -55,15 +38,6 @@ defmodule Rogu.Life do
 
   @doc """
   Updates a log.
-
-  ## Examples
-
-      iex> update_log(log, %{field: new_value})
-      {:ok, %Log{}}
-
-      iex> update_log(log, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def update_log(%Log{} = log, attrs) do
     log
@@ -73,15 +47,6 @@ defmodule Rogu.Life do
 
   @doc """
   Deletes a Log.
-
-  ## Examples
-
-      iex> delete_log(log)
-      {:ok, %Log{}}
-
-      iex> delete_log(log)
-      {:error, %Ecto.Changeset{}}
-
   """
   def delete_log(%Log{} = log) do
     Repo.delete(log)
@@ -89,12 +54,6 @@ defmodule Rogu.Life do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking log changes.
-
-  ## Examples
-
-      iex> change_log(log)
-      %Ecto.Changeset{source: %Log{}}
-
   """
   def change_log(%Log{} = log) do
     Log.changeset(log, %{})
